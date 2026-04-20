a467058ed5ffb215475e9664e7f24c2203f82a44
Noraa <noha0002@stud.kea.dk>
2026-04-20 13:06:15 +0200

    product-list & image tilføjelse

---
+++ src/components2/FetchProducts.jsx
@@ -0,0 +1,41 @@
+import TheProducts from "./TheProducts"
+
+const FetchProducts = () => {
+    return ( 
+       <div>
+        <MyProducts/>
+       </div>
+     );
+}; 
+
+const MyProducts = async() =>{
+    "use server"; 
+    const url = "https://dummyjson.com/products/category/furniture";
+ 
+ const response = await fetch(url);
+ 
+  const OurProducts = await response.json();
+  
+
+  return OurProducts.products.map((product) => {
+    return (
+      <TheProducts
+        key={product.id}
+        title={product.title}
+        price={product.price}
+        image={product.images[0]}
+      />
+    );
+  });
+
+  
+}; 
+
+
+
+
+
+
+
+ 
+export default FetchProducts;
