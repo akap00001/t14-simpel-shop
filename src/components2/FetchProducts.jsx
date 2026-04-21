bcc35f74bfaf23a27f1bb1a4340718c99d9144cc
Noraa <noha0002@stud.kea.dk>
2026-04-21 11:14:09 +0200

    billeder

--- src/components2/FetchProducts.jsx
+++ src/components2/FetchProducts.jsx
@@ -8,29 +8,35 @@ const FetchProducts = () => {
      );
 }; 
 
-const MyProducts = async() =>{
-    "use server"; 
-    const url = "https://dummyjson.com/products/category/furniture";
- 
- const response = await fetch(url);
- 
-  const OurProducts = await response.json();
-  
+const MyProducts = async () => {
+  "use server";
 
-  return OurProducts.products.map((product) => {
-    return (
-      <TheProducts
-        key={product.id}
-        title={product.title}
-        price={product.price}
-        image={product.images[0]}
-      />
-    );
-  });
+  const url1 = "https://dummyjson.com/products/category/furniture";
+  const url2 = "https://dummyjson.com/products/category/home-decoration";
+
+  const res1 = await fetch(url1);
+  const res2 = await fetch(url2);
+
+  const data1 = await res1.json();
+  const data2 = await res2.json();
 
   
-}; 
+  const allProducts = [...data1.products, ...data2.products];
 
+  return (
+    <div className="grid">
+      {allProducts.map((product) => (
+        <TheProducts
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          discountPercentage={product.discountPercentage}
+          image={product.images[0]}
+        />
+      ))}
+    </div>
+  );
+};
 
 
 
--- src/components2/TheProducts.jsx
+++ src/components2/TheProducts.jsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
+import { MdArrowForwardIos } from "react-icons/md";
 
-const TheProducts = ({ title, price, image }) => {
+
+
+
+
+const TheProducts = ({ title, price, image, discountPercentage }) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{price}</p>
-      <Image src={image} alt="Picture of furniture" width={220} height={220} />
+      <Image
+        src={image}
+        alt={title}
+        width={220}
+        height={220}
+        className="border"
+      />
+      <div className="text-wrap">
+        <h1>{title}</h1>
+
+        <p>
+          <span className="price-red">${price}</span>
+          <br />
+          <span className="discount"> {discountPercentage}%</span>
+        </p>
+      </div>
+      <MdArrowForwardIos 
+ alt={title} size={20} className="arrow" />
     </div>
   );
 };
